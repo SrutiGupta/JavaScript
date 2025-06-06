c69ab26b0c55c5247f9b3cf7dff569e4ac1a8990
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-06-06 15:01:38 +0000

    object and array done

---
+++ array/array2.js
@@ -0,0 +1,10 @@
+const array1=["thor","baby"]
+const array2=["All","piku"]
+
+//spread 
+const allvalue=[...array1,...array2]
+console.log(allvalue)
+//it is a array or not
+console.log(Array.isArray("sruti"))
+//string intersting
+console.log(Array.from({name: "sruti"}))
