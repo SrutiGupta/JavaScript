69a4acdf0222e1dec61a01d9acabfc3a662fdb7b
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-06-06 16:40:14 +0000

    total object done

---
+++ objects/object2.js
@@ -0,0 +1,13 @@
+
+const course ={
+    coursename:"js",
+    price :"50",
+    courseInstructor : "hellow"
+
+}
+//we can also do these by course.couseinstructor
+//destruct
+const {courseInstructor : instructor} = course;
+console.log(instructor)
+
+//API
