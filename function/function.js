2efac42e34eb5588571bb3bf6cc58a6547ce6a62
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-06-07 07:04:15 +0000

    Total functions done

--- function/function.js
+++ function/function.js
@@ -19,3 +19,33 @@ function loginuser(username)
 }
 
 console.log(loginuser("sruti"))
+
+//shopping card 
+
+function calculateprice(...num1)//rest 
+{
+    return num1
+}
+console.log(calculateprice(200,500,300))
+
+function calculate(val1,val2,...num1)//rest 
+{
+    return num1
+}
+console.log(calculate(200,500,300))
+
+
+//for object 
+
+const user= {
+   name : "sruti",
+   age :20,
+   roll_no :20233057
+}
+
+function objectdefine(anyobject)
+{
+    console.log(`The name of the user is ${anyobject.name} and the age of user is ${anyobject.age}`)
+}
+
+objectdefine(user)
