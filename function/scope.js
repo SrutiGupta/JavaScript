cd1d99567df4925a14e272d24a443cac5b280dae
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-06-07 07:23:51 +0000

    basics of scope

---
+++ function/scope.js
@@ -0,0 +1,14 @@
+let a=10
+const b=30//global scope
+//var c=20//not to declare 
+
+//scope { } of the program
+
+//not to use var because if we create a variable in the function using var and didn't decalre in out of the scope but they are still be in the outer scope s...so if we work on any group project so it is very dangereous !!
+/*if (true)
+{
+    var c=20 //local scope
+}
+console.log(c)*/
+
+
