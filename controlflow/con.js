69ed13da796fb9c1480b9428d6ddb00903dd36ad
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-07-22 08:51:20 +0000

    done

---
+++ controlflow/con.js
@@ -0,0 +1,24 @@
+//>,<,!=,<=,>=,==
+//=== , !== type and value both checks(imp)
+/*if(2==4)
+{
+    console.log("hello")
+}
+else
+{
+   console.log ("hiii")
+}*/
+
+//&& :- if any one false return false 
+//|| :- if any one true return true
+
+
+//switch case :
+switch(5)
+{
+    case 1:
+        console.log('january');
+        break;
+    default :
+        console.log('break');
+}
